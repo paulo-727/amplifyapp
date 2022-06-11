1d03ba19b491184e9fe131abf19e893934c066fc
paulo-727 <103083162+paulo-727@users.noreply.github.com>
2022-06-10 21:44:26 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,234 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API, Storage } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from "@material-ui/icons/Add";
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import { Fab } from "@material-ui/core";
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+    margin: theme.spacing(3),
+  },
+  itens: {
+    width: '90%',
+    margin: '10px',
+    padding: '10px',
+  },
+  inline: {
+    display: 'inline',
+  },
+  input: {
+    display: 'none',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: '25ch',
+    color: "#2882F8",
+  },
+  div: {
+    color: "#FFF",
+    backgroundColor: "#f29316",
+    margin: '10px',
+    marginBottom: '20px',
+    padding: '15px',
+  },
+  campos: {
+    margin: '10px',
+    padding: '10px',
+  },
+  camera: {
+    color: "#f29316",
+  }
+}));
+
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }  
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
+  const classes = useStyles();
+  
   return (
+    
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      <Box boxShadow={4} width="auto">
+      <div className={classes.div}>
+      <Typography variant="h4" component="h4" >
+        Listagem de notas
+      </Typography>
+      </div>
+      <div className={classes.campos}>
+         <TextField 
+            id="filled-basic" 
+            label="Título"
+            variant="filled" 
+            className={classes.textField}
+            onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+            placeholder="Título"
+            value={formData.name}
+          />
+
+         <TextField 
+            id="filled-basic" 
+            label="Descrição"
+            variant="filled" 
+            className={classes.textField}
+            onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+            placeholder="Descrição"
+            value={formData.description}
+          />
+
+        <input accept="image/*" className={classes.input} id="icon-button-file" type="file" onChange={onChange}/>
+        <label htmlFor="icon-button-file">
+          <IconButton className={classes.camera} aria-label="upload picture" component="span">
+            <PhotoCamera />
+          </IconButton>
+        </label>
+{/*
+        <label htmlFor="upload-photo">
+          <input
+            style={{ display: "none" }}
+            id="upload-photo"
+            name="upload-photo"
+            type="file"
+            onChange={onChange}
+          />
+          <Fab
+            color="secondary"
+            size="small"
+            component="span"
+            aria-label="add"
+            variant="extended">
+            <AddIcon /> Procurar imagem
+          </Fab>
+        </label>
+ */}       
+        <Fab 
+          className={classes.camera}
+          size="small" 
+          component="span" 
+          aria-label="add" 
+          onClick={createNote}
+          >
+          <AddIcon />
+        </Fab>
+
+      </div>
+     
+          <List className={classes.itens}>
+          <Grid item xs={12} md={12}>         
+          {
+            notes.map(note => (
+              <div key={note.id || note.name}>
+              <ListItem alignItems="flex-start">
+              
+                  <ListItemAvatar>
+                  {
+                    note.image &&
+                  <Avatar  src={note.image} alt=""/>
+                  }
+                  </ListItemAvatar>
+
+                  <ListItemText
+                    primary={note.name}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                        >
+                          {note.description}
+                        </Typography>
+                        
+                      </React.Fragment>
+                    }
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete" onClick={() => deleteNote(note)} variant="contained" color="secondary">
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+              </ListItem>
+              <Divider variant="inset" component="li" /> 
+            </div>
+            ))
+          }
+          
+          </Grid>
+          </List>
+          <AmplifySignOut />
+     </Box>
     </div>
+    
   );
 }
 
